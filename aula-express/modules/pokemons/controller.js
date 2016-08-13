3460affc49cca312c9b7658e109d0b048f3679a3
Faculdade Alfa <faculdadealfa@Mac-11.local>
2016-08-13 14:12:53 -0300

    Eventos

--- aula-express/modules/pokemons/controller.js
+++ aula-express/modules/pokemons/controller.js
@@ -1,42 +1,30 @@
-'use strict'
-const PokemonModel = require('./model')
 
+'use strict';
+const Model = require('./model');
 const Controller = {
-	create: (req, res) => {
-		let mod = req.body
-		PokemonModel.create(mod, function (err, data) {
-			if (err) return console.log('ERRO: ', err);
-			res.json(data);
-		})
-	},
-	find:(req, res) => {
-		query = {};
-		PokemonModel.find(query, function (err, data) {
-			if (err) return console.log('ERRO: ', err);
-			res.json(data);
-		})
-	},
-	findOne:(req,res) => {
-		query = {_id:req.params.id};
-		PokemonModel.findOne(query, function (err, data) {
-			if (err) return console.log('ERRO: ', err);
-			res.json(data);
-		})
-	},
-	update:(req, res) => {
-		query = {_id:req.params.id};
-		let mod = req.body
-		PokemonModel.update(query, mod, function (err, data) {
-			if (err) return console.log('ERRO: ', err);
-			res.json(data);
-		})
-	},
-	remove:(req, res) => { 
-		query = {_id:req.params.id};
-		PokemonModel.remove(query, function (err, data) {
-			if (err) return console.log('ERRO: ', err);
-			res.json(data);
-		})
-	}
-}
-module.exports = Controller
+  create: function(data,callback) {
+    const model = new Model(data);
+    model.save(callback);
+  },
+  find: function(query,callback) {
+    Model.find(query,callback);
+  },
+  update: function(query, mod, options) {
+    options = options || {};
+    Model.update(query, mod, options, function (err, data) {
+      if (err) {
+        return console.log('ERRO: ', err);
+      }
+      return console.log('Alterou:', data);
+    });
+  },
+  delete: function(query) {
+    Model.remove(query, function (err, data) {
+      if (err)  { 
+        return console.log('ERRO: ', err);
+      }
+      return console.log('Deletou:', data);
+    });
+  },
+};
+module.exports = Controller;
--- aula-express/modules/pokemons/routes.js
+++ aula-express/modules/pokemons/routes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const Controller = require('./controller')
+const CRUD = require('./crud')
 
 router.get('/', Controller.find) 
-router.get('/:id', Controller.findOne) 
+//router.get('/:id', Controller.findOne) 
 router.post('/', Controller.create) 
-router.put('/:id', Controller.update) 
-router.delete('/:id', Controller.remove) 
+//router.put('/:id', Controller.update) 
+//router.delete('/:id', Controller.remove) 
 
 module.exports = router;
