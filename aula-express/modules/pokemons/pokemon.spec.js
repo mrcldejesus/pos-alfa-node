8fabc5166f683189de9d1b2fb9cb728649a822f3
mrcldejesus <marcelinho.trx@hotmail.com>
2016-08-15 20:55:08 -0300

    Update pokemon.spec.js

--- aula-express/modules/pokemons/pokemon.spec.js
+++ aula-express/modules/pokemons/pokemon.spec.js
@@ -2,12 +2,10 @@ require('./../../db/config.test')
 const assert = require('assert');
 const Controller = require('./controller')
 
-
-describe('Pokemon Controller', ()=> {
-
-	before ((done)=>{
-		Controller.remove({}, (err, data)=>done())
-	})
+describe('Pokemon Controller', () => {
+	before((done) => {
+		Controller.remove({}, (err, data) => done())
+	});
 
 	var msg1 = 'Quando iniciamos sem pokemons a lista deve vir vazia'
 	describe(msg1, () => {
@@ -20,13 +18,12 @@ describe('Pokemon Controller', ()=> {
 				done()
 			}
 			Controller.find(query, callback)
-		})
-	})
+		});
+	});
 
-	var msg1 = 'Quando iniciamos sem pokemons a lista deve vir vazia'
 	describe(msg1, () => {
 		it('Deve retornar array vazio', (done) => {
-			var query = {}
+			var query = {name:'Marcelo'}
 			var callback = (err, data) => {
 				console.log('data', data)
 				assert.equal(null, err,  'Erro não é nulo')
@@ -34,26 +31,48 @@ describe('Pokemon Controller', ()=> {
 				done()
 			}
 			Controller.findOne(query, callback)
-		})
-	})
+		});
+	});
 
-
-	describe('CREATE', () => {
+	describe('MENSAGEM CREATE', (done) => {
 		it('No create o retorno deve ser o mesmo objeto enviado, adicionando _id', (done) => {
 			var mod = {
-				name: 'teste',
-				attack: 9001,
-				defense: 8001
+				name: 'Novo',
+				attack: 8000,
+				defense: 9000
 			}
-
 			var callback = (err, data) => {
-				//assert.equal(mod, data, 'Lista veio vazia')	
+				assert.notEqual(undefined,data._id)
 				assert.Equal('object', typeof data._id)
 				done()
 			}
-			Controller.create(mod, callback)
-		})
-	})
+			Controller.create(mod, callback);
+		});
+	});
+
+	describe('MENSAGEM DELETAR',()=>{
+		it('Quando deletar {"ok":1,"n":1}', (done)=>{
+			var query = {}
+			var callback = (err,data) => {
+				assert.equal(1, data.ok)
+				assert.equal(true, (data.n>0))
+				done()
+			}
+			Controller.remove(qry,callback);
+		});
+	});
+
+	describe('MENSAGEM UPDATE',()=>{
+		it('Deve retornar um objeto', (done)=>{
+			var query = {name:'NOVO NOME'}
+			var callback = (err,data) => {
+				assert.equal(null,err,'Não é nulo')
+				assert.equal(0,data.length,'Lista não veio vazia...')
+				done()
+			}
+			Controller.update(query,callback);
+		});
+	}); 
 })
 
 //Routes -> crud -> controller -> model -> schema -> validation
